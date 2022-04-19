9d43ecbd73c340c4992fb2c832f4e6573115122c
Cr2do <credoay@gmail.com>
2022-04-19 23:59:59 +0100

    add convert p function  and testing

--- convert_strings.c
+++ convert_strings.c
@@ -63,3 +63,30 @@ unsigned int c_percent(va_list args __attribute__((unused)), buffer_t *output)
 
 	return (len);
 }
+
+/**
+ * convert_p - convert argument to an adress => in progress
+ * @args: list of arguments
+ * @output: the result output
+ *
+ * Return: the number of bytes stored in buffer
+ */
+unsigned int convert_p(va_list args, buffer_t *output)
+{
+	unsigned int len = 0, addr;
+	char *null = "(null)";
+
+	addr = va_arg(args, unsigned int);
+
+	if (addr == '\0')
+		return (_memcpy(output, null, 6));
+
+	/*
+	* to change parcourir la liste d'arguments et
+	* formatter suivant une base donnee
+	*
+	*/
+	len += convert_base(addr, 16, "0123456789abcdef", output);
+
+	return (len);
+}
