debede347917e23852ef01c9a47646d19f83e816
Cr2do <credoay@gmail.com>
2022-04-20 00:01:09 +0100

    define structure of r and R function

---
+++ convert_special_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * convert_r - convert arguments
+ * @args: list of arguments
+ * @output: the result output
+ *
+ * Return: the number of bytes stored in buffer
+ */
+unsigned int convert_r(va_list args, buffer_t *output)
+{
+
+/*TODO*/
+
+}
+
+/**
+ * convert_R - convert arguments
+ * @args: list of arguments
+ * @output: the result output
+ *
+ * Return: the number of bytes stored in buffer
+ */
+unsigned int convert_R(va_list args, buffer_t *output)
+{
+
+/*TODO*/
+
+}
